69b8421a50ebcc2bec9b7d369846565ee16c3b90
Pär Björklund <per@ohmy.nu>
2016-03-05 14:15:36 +0100

    quick and dirty fix to work with native promise

--- promise_server.js
+++ promise_server.js
@@ -1,28 +1,29 @@
 var assert = require("assert");
 var fiberPool = require("./fiber_pool.js").makePool();
-var MeteorPromise = require("./promise.js");
+//var MeteorPromise = require("./promise.js");
+var asap = require('asap');
 
 // Replace MeteorPromise.prototype.then with a wrapper that ensures the
 // onResolved and onRejected callbacks always run in a Fiber.
-var es6PromiseThen = MeteorPromise.prototype.then;
-MeteorPromise.prototype.then = function (onResolved, onRejected) {
-  var Promise = this.constructor;
+var es6PromiseThen = Promise.prototype.then;
+Promise.prototype.then = function (onResolved, onRejected) {
+  var promise = this.constructor;
 
-  if (typeof Promise.Fiber === "function") {
-    var fiber = Promise.Fiber.current;
+  if (typeof promise.Fiber === "function") {
+    var fiber = promise.Fiber.current;
     var dynamics = cloneFiberOwnProperties(fiber);
 
     return es6PromiseThen.call(
       this,
-      wrapCallback(onResolved, Promise, dynamics),
-      wrapCallback(onRejected, Promise, dynamics)
+      wrapCallback(onResolved, promise, dynamics),
+      wrapCallback(onRejected, promise, dynamics)
     );
   }
 
   return es6PromiseThen.call(this, onResolved, onRejected);
 };
 
-function wrapCallback(callback, Promise, dynamics) {
+function wrapCallback(callback, promise, dynamics) {
   if (! callback) {
     return callback;
   }
@@ -32,7 +33,7 @@ function wrapCallback(callback, Promise, dynamics) {
       callback: callback,
       args: [arg], // Avoid dealing with arguments objects.
       dynamics: dynamics
-    }, Promise);
+    }, promise);
   };
 }
 
@@ -71,23 +72,23 @@ function shallowClone(value) {
 
 // Yield the current Fiber until the given Promise has been fulfilled.
 function await(promise) {
-  var Promise = promise.constructor;
-  var Fiber = Promise.Fiber;
+  var newPromise = Promise.constructor;
+  var fiber = Promise.Fiber;
 
   assert.strictEqual(
-    typeof Fiber, "function",
+    typeof fiber, "function",
     "Cannot await unless Promise.Fiber is defined"
   );
 
-  var fiber = Fiber.current;
+  var currentFiber = fiber.current;
 
   assert.ok(
-    fiber instanceof Fiber,
+    currentFiber instanceof fiber,
     "Cannot await without a Fiber"
   );
 
-  var run = fiber.run;
-  var throwInto = fiber.throwInto;
+  var run = currentFiber.run;
+  var throwInto = currentFiber.throwInto;
 
   if (process.domain) {
     run = process.domain.bind(run);
@@ -97,12 +98,12 @@ function await(promise) {
   // The overridden es6PromiseThen function is adequate here because these
   // two callbacks do not need to run in a Fiber.
   es6PromiseThen.call(promise, function (result) {
-    tryCatchNextTick(fiber, run, [result]);
+    tryCatchNextTick(currentFiber, run, [result]);
   }, function (error) {
-    tryCatchNextTick(fiber, throwInto, [error]);
+    tryCatchNextTick(currentFiber, throwInto, [error]);
   });
 
-  return Fiber.yield();
+  return fiber.yield();
 }
 
 // Invoke method with args against object in a try-catch block,
@@ -118,38 +119,37 @@ function tryCatchNextTick(object, method, args) {
   }
 }
 
-MeteorPromise.awaitAll = function (args) {
+Promise.awaitAll = function (args) {
   return await(this.all(args));
 };
 
-MeteorPromise.await = function (arg) {
+Promise.await = function (arg) {
   return await(this.resolve(arg));
 };
 
-MeteorPromise.prototype.await = function () {
+Promise.prototype.await = function () {
   return await(this);
 };
 
 // Return a wrapper function that returns a Promise for the eventual
 // result of the original function.
-MeteorPromise.async = function (fn, allowReuseOfCurrentFiber) {
-  var Promise = this;
+Promise.async = function (fn, allowReuseOfCurrentFiber) {
+  var promise = this;
   return function () {
-    return Promise.asyncApply(
+    return promise.asyncApply(
       fn, this, arguments,
       allowReuseOfCurrentFiber
     );
   };
 };
 
-MeteorPromise.asyncApply = function (
+Promise.asyncApply = function (
   fn, context, args, allowReuseOfCurrentFiber
 ) {
-  var Promise = this;
-  var Fiber = Promise.Fiber;
-  var fiber = Fiber && Fiber.current;
+  var promise = this;
+  var currentFiber = promise.Fiber && promise.Fiber.current;
 
-  if (fiber && allowReuseOfCurrentFiber) {
+  if (currentFiber && allowReuseOfCurrentFiber) {
     return this.resolve(fn.apply(context, args));
   }
 
@@ -157,20 +157,79 @@ MeteorPromise.asyncApply = function (
     callback: fn,
     context: context,
     args: args,
-    dynamics: cloneFiberOwnProperties(fiber)
-  }, Promise);
+    dynamics: cloneFiberOwnProperties(currentFiber)
+  }, promise);
 };
 
 Function.prototype.async = function (allowReuseOfCurrentFiber) {
-  return MeteorPromise.async(this, allowReuseOfCurrentFiber);
+  return Promise.async(this, allowReuseOfCurrentFiber);
 };
 
 Function.prototype.asyncApply = function (
   context, args, allowReuseOfCurrentFiber
 ) {
-  return MeteorPromise.asyncApply(
+  return Promise.asyncApply(
     this, context, args, allowReuseOfCurrentFiber
   );
 };
+Promise.denodeify = function (fn, argumentCount) {
+  argumentCount = argumentCount || Infinity;
+  return function () {
+    var self = this;
+    var args = Array.prototype.slice.call(arguments, 0,
+        argumentCount > 0 ? argumentCount : 0);
+    return new Promise(function (resolve, reject) {
+      args.push(function (err, res) {
+        if (err) reject(err);
+        else resolve(res);
+      })
+      var res = fn.apply(self, args);
+      if (res &&
+        (
+          typeof res === 'object' ||
+          typeof res === 'function'
+        ) &&
+        typeof res.then === 'function'
+      ) {
+        resolve(res);
+      }
+    })
+  }
+}
+Promise.nodeify = function (fn) {
+  return function () {
+    var args = Array.prototype.slice.call(arguments);
+    var callback =
+      typeof args[args.length - 1] === 'function' ? args.pop() : null;
+    var ctx = this;
+    try {
+      return fn.apply(this, arguments).nodeify(callback, ctx);
+    } catch (ex) {
+      if (callback === null || typeof callback == 'undefined') {
+        return new Promise(function (resolve, reject) {
+          reject(ex);
+        });
+      } else {
+        asap(function () {
+          callback.call(ctx, ex);
+        })
+      }
+    }
+  }
+}
 
-module.exports = exports = MeteorPromise;
+Promise.prototype.nodeify = function (callback, ctx) {
+  if (typeof callback != 'function') return this;
+
+  this.then(function (value) {
+    asap(function () {
+      callback.call(ctx, null, value);
+    });
+  }, function (err) {
+    asap(function () {
+      callback.call(ctx, err);
+    });
+  });
+}
+
+module.exports = exports = Promise;
